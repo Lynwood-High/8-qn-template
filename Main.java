f76ca134a2901c275b8c84c088e4382b3dade6ed
AashikaVasra <70162923+AashikaVasra@users.noreply.github.com>
2020-11-11 21:58:58 -0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+/** For Teaching Team **
+Reviewer: 
+Feedback:
+
+**/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Question 1");
+
+    System.out.println("\nQuestion 2");
+
+    System.out.println("\nQuestion 3 Part 1");
+
+    System.out.println("\nQuestion 3 Part 2");
+
+    System.out.println("\nQuestion 4");
+
+    System.out.println("\nQuestion 5 Part 1");
+
+    System.out.println("\nQuestion 5 Part 2");
+  }
+}
